29dab831758ff2b4a8071b181e1cf3d554a3e8c8
Sankalp <sankalp123427@gmail.com>
2020-08-29 19:56:46 +0530

    updating sort.py

--- dsalgo/sort.py
+++ dsalgo/sort.py
@@ -1,29 +1,30 @@
-def sort(array,algo=None,reverse=False):
-	if algo==None:
-		return sorted(array,reverse=reverse)
-	else:
-		return exec(algo+'(reverse)')
+class Sort:
+    def __init__(self, array):
+        self.array = array
+        
+    def bubble(self, reverse=False):
+        '''
+        A bubble sort algorithm is an algorithm
+        that repeatedly swaps adjacent elements.
+        The smallest values comes in front and 
+        large values goes at back, similar to that a
+        lighter bubbles comes up, hence bubble sort
 
-def bubble(reverse=False):
-	'''
-	A bubble sort algorithm is an algorithm
-	that repeatedly swaps adjacent elements.
-	The smallest values comes in front and 
-	large values goes at back, similar to that a
-	lighter bubbles comes up, hence bubble sort
-	'''
-	n=len(array)
-	for i in range(n):
-		swap=0
-		for j in range(0,n-i-1):
-			if self.array[j]>self.array[j+1]:
-				self.array[j],self.array[j+1]=self.array[j+1],self.array[j]
-				swap=1
-		if swap==0:
-			break
-	if reverse==True:
-		return self.array[::-1]
-	return self.array
-
-if __name__ == '__main__':
-	sort()
+        args:
+          :array:(list) -> list to be sorted
+          :reverse:(boolean) -> default = False,
+                                can be True for sort 
+                                in reverse order
+        '''
+        n=len(self.array)
+        for i in range(n):
+            swap=0
+            for j in range(0,n-i-1):
+                if self.array[j]>self.array[j+1]:
+                    self.array[j],self.array[j+1]=self.array[j+1],self.array[j]
+                    swap=1
+            if swap==0:
+                break
+        if reverse==True:
+            return array[::-1]
+        return self.array
