d8df6ad23e696527e56a35b9d042307014017c68
sankalp <sankalp123427@gmail.com>
2020-08-29 21:18:23 +0530

    improved sorting

--- dsalgo/sort.py
+++ dsalgo/sort.py
@@ -1,30 +1,67 @@
-class Sort:
-    def __init__(self, array):
-        self.array = array
-        
-    def bubble(self, reverse=False):
-        '''
-        A bubble sort algorithm is an algorithm
-        that repeatedly swaps adjacent elements.
-        The smallest values comes in front and 
-        large values goes at back, similar to that a
-        lighter bubbles comes up, hence bubble sort
+import sys
 
-        args:
-          :array:(list) -> list to be sorted
-          :reverse:(boolean) -> default = False,
-                                can be True for sort 
-                                in reverse order
+class Sort:
+    # def __init__(self, array, algo, reverse=False):
+    #     '''
+    #     args:
+    #       :array: (list) : a python list
+    #       :algo: (str): sorting algorithm type
+    #                         values supported are:
+    #                         1. bubble
+    #       :reverse: (bool) : default = False
+    #                         if True order is reversed.
+    #     return:
+    #         sorted array
+    #     '''
+    #     self.array = array
+    #     self.algo = algo
+    #     self.reverse = reverse
+
+    def __new__(self, array, algo, reverse=False):
         '''
-        n=len(self.array)
-        for i in range(n):
-            swap=0
-            for j in range(0,n-i-1):
-                if self.array[j]>self.array[j+1]:
-                    self.array[j],self.array[j+1]=self.array[j+1],self.array[j]
-                    swap=1
-            if swap==0:
-                break
-        if reverse==True:
-            return array[::-1]
-        return self.array
+        args:
+          :array: (list) : a python list
+          :algo: (str): sorting algorithm type
+                            values supported are:
+                            1. bubble
+          :reverse: (bool) : default = False
+                            if True order is reversed.
+        return:
+            sorted array
+        '''
+        self.array = array
+        self.algo = algo
+        self.reverse = reverse
+
+        if self.algo == 'bubble':
+            return bubble(self.array, self.reverse)
+        else:
+            sys.stderr.write("Error: unsupported sorting algorithm passed!")
+
+        
+def bubble(array, reverse=False):
+    '''
+    A bubble sort algorithm is an algorithm
+    that repeatedly swaps adjacent elements.
+    The smallest values comes in front and 
+    large values goes at back, similar to that a
+    lighter bubbles comes up, hence bubble sort
+
+    args:
+      :array:(list) -> list to be sorted
+      :reverse:(boolean) -> default = False,
+                            can be True for sort 
+                            in reverse order
+    '''
+    n=len(array)
+    for i in range(n):
+        swap=0
+        for j in range(0,n-i-1):
+            if array[j]>array[j+1]:
+                array[j],array[j+1]=array[j+1],array[j]
+                swap=1
+        if swap==0:
+            break
+    if reverse==True:
+        return array[::-1]
+    return array
