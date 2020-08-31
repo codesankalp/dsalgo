c85d3f63bc0f0ee6be5f5f7cf66c3ab8ff5d07c9
=Aryamanz29 <=aryamanz29@gmail.com>
2020-08-31 13:44:49 +0530

    Added sorting algorithms it's test file close #4 added sort algorithms for - merge sort - selection sort - quick sort - bubble recursion sort

---
+++ tests/sort_tests.py
@@ -0,0 +1,16 @@
+
+from dsalgo.sort import Sort
+array=[33,12,3,1,3,4,2,1,5,7,8,9]
+
+#merge,selection,bubble,bubble rec
+
+# Sort(array,'merge')
+
+#for print purpose only!It can remove later
+# Sort(array,'printArray')
+sorted_array=Sort(array,'bubble_recursion',True)
+print(sorted_array)
+
+
+
+
