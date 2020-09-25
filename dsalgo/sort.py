0249cb13b5a728995171411c1a30f43e006e00b5
Przemyslaw Pietras <przemyslaw.pietras@syncron.com>
2020-09-25 08:55:28 +0200

    Add shell sort algorithm

--- dsalgo/sort.py
+++ dsalgo/sort.py
@@ -20,6 +20,8 @@ class Sort:
 
         if self.algo == 'bubble':
             return bubble(self.array, self.reverse)
+        elif self.algo == 'shell':
+            return shell(self.array, self.reverse)
         else:
             sys.stderr.write("Error: unsupported sorting algorithm passed!")
 
@@ -49,4 +51,36 @@ def bubble(array, reverse=False):
             break
     if reverse==True:
         return array[::-1]
+    return array
+
+def shell(array, reverse=False):
+    '''
+    A shell sort algorithm is an algorithm
+    that starts by sorting pairs of elements
+    far apart from each other, then progressively
+    reducing the gap between elements to be compared.
+
+    args:
+      :array:(list) -> list to be sorted
+      :reverse:(boolean) -> default = False,
+                            can be True for sort
+                            in reverse order
+    '''
+    gap = len(array) // 2
+
+    while gap > 0:
+        for i in range(gap, len(array)):
+            tmp = array[i]
+            j = i
+            if reverse:
+                while  j >= gap and array[j-gap] < tmp:
+                    array[j] = array[j-gap]
+                    j -= gap
+            else:
+                while  j >= gap and array[j-gap] > tmp:
+                    array[j] = array[j-gap]
+                    j -= gap
+            array[j] = tmp
+        gap //= 2
+
     return array
