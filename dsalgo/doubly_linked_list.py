abfc7e15c8dafcafa93bee2600e8baae004e3555
Keshav <56075233+keshavgbpecdelhi@users.noreply.github.com>
2020-10-07 02:02:51 +0530

    items count using items.count
    Title : In append_item(self, data) function increasing self.count by one every time the function called.
    Fixes : Edit only/-
    Closes : #67

--- dsalgo/doubly_linked_list.py
+++ dsalgo/doubly_linked_list.py
@@ -11,19 +11,6 @@ class doubly_linked_list(object):
         self.tail = None
         self.count = 0
 
-    def append_item(self, data):
-        # Append an item 
-        new_item = Node(data, None, None)
-        if self.head is None:
-            self.head = new_item
-            self.tail = self.head
-        else:
-            new_item.prev = self.tail
-            self.tail.next = new_item
-            self.tail = new_item
-
-        self.count += 1
-
     def print_foward(self):
         for node in self.iter():
             print(node)
@@ -41,6 +28,19 @@ class doubly_linked_list(object):
             item_val = current.data
             current = current.next
             yield item_val
+ 
+    def append_item(self, data):
+        # Append an item 
+        new_item = Node(data, None, None)
+        if self.head is None:
+            self.head = new_item
+            self.tail = self.head
+        else:
+            new_item.prev = self.tail
+            self.tail.next = new_item
+            self.tail = new_item
+
+        self.count += 1 #counting number of items
 
 
 # Use items = doubly_linked_list()
@@ -48,3 +48,4 @@ class doubly_linked_list(object):
 # and use print_forward() to print the output as below :
 # items.print_foward() [#1]
 # items.print_backward() [adding a new function #2]
+# print("Number of items of the  Doubly linked list:",items.count)
